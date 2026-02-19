a5fceaf7b2d2a75d4153a954c3e4ffc50e004878
Gustavo Coimbra <whoisgustavolima@gmail.com>
2026-02-19 10:11:40 -0300

    fix: sensitive case

--- app/Http/Requests/UpdateUserRequest.php
+++ app/Http/Requests/UpdateUserRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class UpdateUSerRequest extends FormRequest
+class UpdateUserRequest extends FormRequest
 {
 
     public function authorize(): bool
