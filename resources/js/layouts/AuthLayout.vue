d032676e808ee3be81faebf1dd8a3ade8dc1a741
Gustavo Coimbra <whoisgustavolima@gmail.com>
2026-02-22 01:06:41 -0300

    update

--- resources/js/layouts/AuthLayout.vue
+++ resources/js/layouts/AuthLayout.vue
@@ -9,7 +9,7 @@ import { Moon, Github } from 'lucide-vue-next';
                 <Moon class="text-amber-100" :size="30"/>
                 <span><strong class="text-amber-100">Lua</strong>/desk</span>
             </div>
-            <div class="bg-amber-100 border border-amber-200/50 shadow-lg p-6 w-[400px] max-w-full">
+            <div class="bg-amber-100 border border-amber-200/50 shadow-lg p-6 w-[400px] max-w-[calc(100dvw-20px)]">
                 <slot/>
             </div>
             <div class="flex items-center justify-center gap-3 mt-6 text-amber-100 text-sm">
