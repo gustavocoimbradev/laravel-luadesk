3eb43e2b13c6d4415d7de31ff56fadf29e5896e0
Gustavo Coimbra <whoisgustavolima@gmail.com>
2026-02-22 14:43:35 -0300

    update

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,5 +6,11 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        config(['database.default' => 'sqlite']);
+        config(['database.connections.sqlite.database' => ':memory:']);
+    }
 }
