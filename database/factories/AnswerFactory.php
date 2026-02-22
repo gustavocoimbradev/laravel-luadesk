0f9a8796798bcd67361eea7c44df271e498014ee
Gustavo Coimbra <whoisgustavolima@gmail.com>
2026-02-22 00:47:33 -0300

    update

--- database/factories/AnswerFactory.php
+++ database/factories/AnswerFactory.php
@@ -5,14 +5,15 @@ namespace Database\Factories;
 use App\Models\Ticket;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
-
+use Faker\Factory as Faker;
 class AnswerFactory extends Factory
 {
 
     public function definition(): array
     {
+        $faker = Faker::create();
         return [
-            'content' => fake()->paragraphs(2, true),
+            'content' => $faker->paragraphs(2, true),
             'ticket_id' => Ticket::factory(),
             'user_id' => User::factory()
         ];
--- database/factories/TicketFactory.php
+++ database/factories/TicketFactory.php
@@ -4,15 +4,16 @@ namespace Database\Factories;
 
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
-
+use Faker\Factory as Faker;
 class TicketFactory extends Factory
 {
 
     public function definition(): array
     {
+        $faker = Faker::create();
         return [
-            'title' => fake()->sentence(4),
-            'content' => fake()->paragraphs(3, true),
+            'title' => $faker->sentence(4),
+            'content' => $faker->paragraphs(3, true),
             'user_id' => User::factory(),
         ];
     }
